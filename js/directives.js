49c91397e16bd72adcdad89f9de84d1ff98b5101
william-bishop <admin@the402.net>
2015-07-13 00:04:43 -0700

    moved new message to directive for reuse

--- js/directives.js
+++ js/directives.js
@@ -7,4 +7,20 @@ angular.module('etiMobile.directives', [])
                 element.on('click', function () { element.toggleClass('item-text-wrap'); });
             }
         }
+    })
+
+    .directive('newMessage', function () {
+        return {
+            restrict: 'EA',
+            scope: {
+                model: '='
+            },
+            template: '' +
+            '<label class="new-message item item-input item-stacked-label"> ' +
+            '<span class="input-label">New Message</span> ' +
+            '<textarea ng-model="messageModel"></textarea> ' +
+            '</label>',
+            link: function (scope, element, attrs) {
+            }
+        }
     });
